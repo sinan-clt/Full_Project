5a610eb17410f81e7b4908f9c9ebc862c234c112
sinanclt <sinanahammed313@gmail.com>
2021-05-11 02:40:35 +0530

    hloo

--- second/settings.py
+++ second/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(cx+xu0tpa%8ijn*)+c#a_n_mvj-yigvdibxj_z2uz^qw48ogg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'second.urls'
